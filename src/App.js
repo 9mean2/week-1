2196291d8061ced1b4d7eed07e37169cce5f08d2
9mean2 <o3omoomin@gmail.com>
2023-02-05 08:37:29 +0900

    feat app.js 업데이트ㅎ

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  console.log("id", id);
+  console.log("password", password);
+
+  //id 필드가 변경될 경우
+  const onIdChaneHandler = (event) => {
+    setId(event.target.value);
+  };
+
+  const onPasswordChaneHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        아이디 : <input type="text" value={id} onChange={onIdChaneHandler} />
+      </div>
+      <div>
+        비밀번호 :{" "}
+        <input
+          type="password"
+          value={password}
+          onChange={onPasswordChaneHandler}
+        />
+      </div>
+      <button
+        onClick={() => {
+          alert(
+            `고객님이 입력하신 아이디는 ${id}이며, 패스워드는 ${password}입니다.`
+          );
+          setId("");
+          setPassword("");
+        }}
+      >
+        로그인
+      </button>
     </div>
   );
 }
