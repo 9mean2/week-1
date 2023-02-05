a3374954912c4df90a03d3b813a3b46c1b33a5f8
9mean2 <o3omoomin@gmail.com>
2023-02-05 10:15:42 +0900

    feat 불변성에 대하여

--- src/App.js
+++ src/App.js
@@ -1,43 +1,25 @@
 import React, { useState } from "react";
 
 function App() {
-  const [id, setId] = useState("");
-  const [password, setPassword] = useState("");
-
-  console.log("id", id);
-  console.log("password", password);
-
-  const onIdChaneHandler = (event) => {
-    setId(event.target.value);
-  };
-
-  const onPasswordChaneHandler = (event) => {
-    setPassword(event.target.value);
-  };
-
+  const [obj, setObj] = useState({
+    name: "goomin",
+    age: 21,
+  });
   return (
     <div>
-      <div>
-        아이디 : <input type="text" value={id} onChange={onIdChaneHandler} />
-      </div>
-      <div>
-        비밀번호 :{" "}
-        <input
-          type="password"
-          value={password}
-          onChange={onPasswordChaneHandler}
-        />
-      </div>
+      <div>{obj.name}</div>;
       <button
         onClick={() => {
-          alert(
-            `고객님이 입력하신 아이디는 ${id}이며, 패스워드는 ${password}입니다.`
-          );
-          setId("");
-          setPassword("");
+          obj.name = "moomin";
+          const obj2 = { ...obj };
+          setObj(obj2);
+          //새로운 객체를 만들어야함
+          // obj.name = "moomin";
+          // console.log(obj);
+          // setObj(obj);
         }}
       >
-        로그인
+        변경
       </button>
     </div>
   );
